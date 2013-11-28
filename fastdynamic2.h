35afae86d0ce23f829d789c6fed9d6cd4707d7a0
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2013-11-28 15:22:58 +0100

    fixed the fastdynamic2.h to work just as the old one but without bucketing

--- fastdynamic2.h
+++ fastdynamic2.h
@@ -1,46 +1,88 @@
 #ifndef _FASTDYNAMIC_H
 #define _FASTDYNAMIC_H
 #include <memory.h>
+#include <stdlib.h>
 #define DEFAULT_CONTAINER_SIZE 4 // use larger numbers when using small objects or you will run out of memory quickly
+// should be 100% compatible with the old fastdynamic one other than setting the size
+// will not delete anything just change the increaseSize variable
 
 template <class T>
 class FastDynamic //array that expands when it runs out of bounds
 {
-    struct bucket
-    {
-        T *contents;
-    };
-    bucket **buckets;
+    T *contents;
 public:
     size_t increaseSize; // in bytes so numnew*contentssize in the SetContainer_size
-    size_t currentLength;
-    size_t contentSize;
+    size_t currentLength; // not how many entries have been written just how many have been allocated
+    size_t currentByteLength;
     FastDynamic(size_t increaseSize)
     {
+        this->increaseSize = increaseSize;
+        contents = (T *)malloc(this->increaseSize * sizeof(T));
+        currentLength = increaseSize;
+        currentByteLength = this->increaseSize * sizeof(T);
+
     }
-    FastDynamic(const FastDynamic<T> &old)
+    FastDynamic(const FastDynamic<T> &old) // this will differ as much as that it will copy while the old one didn't
     {
+        this->increaseSize = old.increaseSize;
+        contents = (T *)malloc(old.currentByteLength);
+        currentLength = old.currentLength;
+        currentByteLength = old.currentByteLength;
+        old.CopyToStatic(contents, currentLength);
     }
     FastDynamic()
     {
+        increaseSize = DEFAULT_CONTAINER_SIZE;
+        contents = (T *)malloc(increaseSize * sizeof(T));
+        currentLength = DEFAULT_CONTAINER_SIZE;
+        currentByteLength = increaseSize * sizeof(T);
     }
-    void SetContainer_size(size_t newsize)
+    void SetContainer_size(size_t newsize) // size is in objects
     {
+        increaseSize = newsize;
     }
     ~FastDynamic()
     {
+        if(contents)
+            free(contents);
     }
     void Clear()
     {
+        if(contents)
+            free(contents);
+
+        contents = (T *)malloc(increaseSize*sizeof(T));
+        currentLength=increaseSize;
+        currentByteLength=increaseSize*sizeof(T);
+    }
     T &operator[](size_t index)
     {
+      if(index >= currentLength)
+        {
+            size_t newsize = (index / increaseSize) + 1;
+            newsize *= increaseSize;
+            //printf("currentLength:%u  newsize:%u index:%u increaseSize:%i\n", currentLength, newsize, index, increaseSize);
+            T *tmp = (T *)realloc(contents, newsize * sizeof(T));
+            contents = tmp;
+            currentLength = newsize;
+            currentByteLength = newsize * sizeof(T);
+        }
+
+        return contents[index];
     }
-    T *GetBucket(int bucketnum)
+    T *GetBucket(int bucketnum) // tbh worthless in this implementation
     {
+        return contents[bucketnum / increaseSize / sizeof(T)];
     }
     void CopyToStatic(T *staticarray, size_t count)
     {
+        if(count == 0)
+            return;
 
+        if(count > currentLength)
+            count = currentLength;
+
+        memcpy(staticarray, contents, sizeof(T)*count);
     }
 };
 #endif
--- test.cpp
+++ test.cpp
@@ -1,13 +1,13 @@
 #include "fast_obj_loader.h"
 #include <time.h>
 #include <stdio.h>
-
+#include "fastdynamic2.h"
 int main(int argc, char *argv[])
 {
     int timestoload = 1;
 
     if(argc == 2)
-        sscanf(argv[1], "%20i", &timestoload);
+    sscanf(argv[1], "%20i", &timestoload);
 
     printf("starting to load file %i times\n", timestoload);
     timespec start, stop;
@@ -16,18 +16,38 @@ int main(int argc, char *argv[])
 
     for(int i = 0; i < timestoload; i++)
     {
-        //obj *testmesh=loadObj("../test.obj");
-        //obj *testmesh = loadObj("../dragon_vrip_res2.obj");
+    //obj *testmesh=loadObj("../test.obj");
+    obj *testmesh = loadObj("../dragon_vrip_res2.obj");
 
-        obj *testmesh=loadObj("../xyzrgb_dragon.obj");
-        //if(testmesh)
-            //writeObj("verification.obj", *testmesh);
+    //obj *testmesh=loadObj("../xyzrgb_dragon.obj");
+    //if(testmesh)
+    //writeObj("verification.obj", *testmesh);
 
-        delete testmesh;
+    delete testmesh;
     }
 
     clock_gettime(CLOCK_REALTIME, &stop);
     calltime = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec) / 1000000000.0;
     printf("done loading file %lfseconds\n", calltime / timestoload);
+
+    //FastDynamic<int> test;
+    //test.SetContainer_size(1024);
+
+    //for(int i = 0; i < 3200000; i++)
+        //test[i] = i;
+
+    //for(int i = 0; i < 3200000; i++)
+        //if(test[i] != i)
+            //printf("wtf");
+
+    //printf("endtest\n");
+
+    //int *staticarray = new int[3200000];
+    //printf("end static test\n");
+    //test.CopyToStatic(staticarray,3200000);
+    //for(int i = 0; i < 3200000; i++)
+        //if(staticarray[i] != i)
+            //printf("wtf");
+    //delete [] staticarray;
     return 0;
 }
